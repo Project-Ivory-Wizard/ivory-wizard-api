e7dea72aebe453e05b9b73a2fa0d51fba6ba0d57
adamxshaohuang <adamhuang.taiwan@gmail.com>
2025-03-24 18:13:28 -0400

    Completed Part 1

--- ivory.wizard.api.Api/Controllers/CatalogController.cs
+++ ivory.wizard.api.Api/Controllers/CatalogController.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using ivory.wizard.api.Domain.Catalog;
 
-namespace ivory.wizard.Api.Controllers
+namespace ivory.wizard.api.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CatalogController : ControllerBase{
+    public class CatalogController : ControllerBase
+    {
         [HttpGet]
-        public IActionResult GetItems(){
+        public IActionResult GetItems()
+        {
             var items = new List<Item>()
             {
                 new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
@@ -15,6 +17,39 @@ namespace ivory.wizard.Api.Controllers
             };
             return Ok(items);
         }
-    }
 
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Item item)
+        {
+            return Created("/catalog/42", item);
+        }
+
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating){
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
+            item.AddRating(rating);
+
+            return Ok(item);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Item item){
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult delete(int id){
+            return NoContent();
+        }
+
+    }
 }
--- ivory.wizard.api.Api/Program.cs
+++ ivory.wizard.api.Api/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
@@ -32,6 +34,9 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
+
+app.MapControllers();
 
 app.Run();
 
