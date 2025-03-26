05738e57dcd2645b3deed89959bd0c3b7ad6fb96
Andre Scutt <andreasscutttmf@gmail.com>
2025-03-26 13:50:45 -0400

    Update StoreContext.cs

--- ivory.wizard.api.Data/StoreContext.cs
+++ ivory.wizard.api.Data/StoreContext.cs
@@ -8,8 +8,11 @@ namespace ivory.wizard.api.Data
         public StoreContext(DbContextOptions<StoreContext> options)
         : base(options)
         {  }
+        
         public DbSet<Item> Items { get; set; }
+        
         public DbSet<Order> Orders { get; set; }
+        }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
